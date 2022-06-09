3e0703c18a4cf3690f193a3c0d1c7dcb9ffe31aa
ycl <a7862725@qq.com>
2022-06-10 00:12:55 +0800

    去除lomlok

--- src/main/java/com/example/study/linkedList/Node.java
+++ src/main/java/com/example/study/linkedList/Node.java
@@ -1,8 +1,6 @@
 package com.example.study.linkedList;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+
 
 /**
  * @Author: yuanchenglong
@@ -11,9 +9,6 @@ import lombok.NoArgsConstructor;
  */
 
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class Node {
     /**
      * 数据域
@@ -23,4 +18,28 @@ public class Node {
      * 指针域
      */
     Node next;
+
+    public Node() {
+    }
+
+    public Node(Integer data, Node next) {
+        this.data = data;
+        this.next = next;
+    }
+
+    public Integer getData() {
+        return data;
+    }
+
+    public void setData(Integer data) {
+        this.data = data;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
 }
