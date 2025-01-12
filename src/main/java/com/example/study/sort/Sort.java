0201d70da7244854ecfaedc484ac785ef5b48ed0
yuanchenglong <yuanchenglong@supermap.com>
2025-01-12 19:52:25 +0800

    优化：冒泡排序优化

--- src/main/java/com/example/study/sort/Sort.java
+++ src/main/java/com/example/study/sort/Sort.java
@@ -11,16 +11,16 @@ public class Sort {
 
 
     /**
-     * 冒泡排序-实现思路：将无序区间的数据依次进行两两比较，不满足条件则替换位置，这里以升序为例，如果a[i]>[a[i+1]，则替换位置，一轮下来之后
+     * 冒泡排序-实现思路：比较相邻元素,大的元素会交换到末尾，不满足条件则替换位置，这里以升序为例，如果a[i]>[a[i+1]，则替换位置，一轮下来之后
      * ，数值最大的数据将放在末尾，  优化：以后每轮比较，无需遍历至前面 已经冒泡后的 数了
      */
     @Test
     public void BubbleSort(){
         int[] arr = new int[]{6,5,7,8,9,1,3,2};
 
-        for (int i = 0; i < arr.length; i++) {
+        for (int i = 0; i < arr.length-1; i++) {
             // 注意：这里-i 是因为已经冒泡后的数据，不需要再比较了
-            for (int j = 0; j < arr.length-1-i; j++) {
+            for (int j = 0; j < arr.length-(i+1); j++) {
                 if(arr[j] > arr[j+1]){
                     swap(arr,j,j+1);
                 }
